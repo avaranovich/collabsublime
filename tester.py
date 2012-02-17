b8266acf06a9721fe5b00906dc64219d813d8380
andrei varanovich <dotnetby@gmail.com>
2012-02-17 17:31:43 +0100

    the program should not be closed while there is a communication with agent

--- tester.py
+++ tester.py
@@ -8,7 +8,7 @@ from threading import Thread
 class JsonComposer:
 	def __init__(self):
 		self.host = 'localhost'
-		self.port = int(open('../cccp/agent/dist/cccp.port', 'r').read())
+		self.port = int(open('../cccp_fork/agent/dist/cccp.port', 'r').read())
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.connected = False
 		self.filename = ""
@@ -30,13 +30,12 @@ class JsonComposer:
 		if wait:
 			data = self.s.recv(1024)
 			print 'Received', repr(data)
-		self.s.close()
 		return True
     
 	def initConnectionJson(self):
 		cid = self.callId()
 		print cid
-		return ({"swank":"init-connection", "args":[{"protocol": "http", "host": "slowb127.uni-koblenz.de", "port" : 8585}], "callId" : self.callId()})
+		return ({"swank":"init-connection", "args":[{"protocol": "http", "host": "localhost", "port" : 8585}], "callId" : self.callId()})
     
 	def linkFileJson(self):
 		return ({"swank":"link-file", "args":[{"id": "id", "file-name": self.filename }], "callId" : self.callId()})
@@ -50,5 +49,9 @@ class JsonComposer:
 jc = JsonComposer()
 jc.filename = "foo.txt"
 print json.dumps(jc.initConnectionJson())
-#jc.rpcSend(json.dumps(jc.initConnectionJson()), False)
+jc.rpcSend(json.dumps(jc.initConnectionJson()), False)
+
+x = raw_input("please type OK to exit: ")
+if(x == "OK"):
+	exit(0)
 
