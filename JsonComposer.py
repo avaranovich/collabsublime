ce17676f5627149283c1ee19c0d960ae89b1f5e2
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2012-02-19 15:28:14 +0100

    :

---
+++ JsonComposer.py
@@ -0,0 +1,22 @@
+class JsonComposer:
+	def __init__(self):
+		self.filename = ""
+		self.callid = 0
+    
+	def callId(self):
+		self.callid = self.callid + 1      
+		return self.callid
+
+	def initConnectionJson(self):
+		cid = self.callId()
+		print cid
+		return ({"swank":"init-connection", "args":[{"protocol": "http", "host": "localhost", "port" : 8585}], "callId" : self.callId()})
+    
+	def linkFileJson(self):
+		return ({"swank":"link-file", "args":[{"id": "id", "file-name": self.filename }], "callId" : self.callId()})
+    
+	def unlinkFileJson(self):
+		return ({"swank":"unlink-file", "args":[{ "file-name": self.filename }], "callId" : self.callId()})
+    
+	def editFileJson(self, op, pos, s):
+		return ({"swank": "edit-file", "file-name": self.filename, "args":[{"retain": pos}, { op: s}], "callId" : self.callId()})
--- tester.py
+++ tester.py
@@ -4,54 +4,8 @@ import os
 import json
 import socket, asyncore
 from threading import Thread
-
-class JsonComposer:
-	def __init__(self):
-		self.host = 'localhost'
-		self.port = int(open('../cccp/agent/dist/cccp.port', 'r').read())
-		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.connected = False
-		self.filename = ""
-		self.callid = 0
-    
-	def callId(self):
-		self.callid = self.callid + 1      
-		return self.callid
-	
-   	def rpcSend(self, msg, wait):
-		if not self.connected:
-			self.s.connect((self.host, self.port)) 
-			self.connected = True
-		hexed = "0000" + hex(len(msg))[2:]
-		toSend = hexed + msg
-		print "Sendisng: " + toSend
-		self.s.send(toSend)
-		if wait:
-			data = self.s.recv(1024)
-			print 'Received', repr(data)
-		return True
-    
-	def initConnectionJson(self):
-		cid = self.callId()
-		print cid
-		return ({"swank":"init-connection", "args":[{"protocol": "http", "host": "localhost", "port" : 8585}], "callId" : self.callId()})
-    
-	def linkFileJson(self):
-		return ({"swank":"link-file", "args":[{"id": "id", "file-name": self.filename }], "callId" : self.callId()})
-    
-	def unlinkFileJson(self):
-		return ({"swank":"unlink-file", "args":[{ "file-name": self.filename }], "callId" : self.callId()})
-    
-	def editFileJson(self, op, pos, s):
-		return ({"swank": "edit-file", "file-name": self.filename, "args":[{"retain": pos}, { op: s}], "callId" : self.callId()})
-
-#old sync verion
-#jc = JsonComposer()
-#jc.filename = "foo.txt"
-#print json.dumps(jc.initConnectionJson())
-#jc.rpcSend(json.dumps(jc.initConnectionJson()), False)
-#jc.rpcSend(json.dumps(jc.unlinkFileJson()), False)
-from AgentClient import *
+from AgentClient import AgentClient
+from JsonComposer import JsonComposer
 
 def itsdone(result):
   print "Done! result=%r" % (result)
